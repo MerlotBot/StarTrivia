9bf0df4f34f28e9c3d36f7c099ee6dfd6ca61ab4
Nikolaos Antonakakis <nikolaos.antonakakis@wu.ac.at>
2019-03-24 22:24:50 +0100

    Homeworld Data complete. (minor issue with terrain text  being cut off the screen)

---
+++ StarTrivia/Model/Homeworld.swift
@@ -0,0 +1,17 @@
+//
+//  Homeworld.swift
+//  StarTrivia
+//
+//  Created by Antonakakis Nikolaos on 24.03.19.
+//  Copyright © 2019 Antonakakis Nikolaos. All rights reserved.
+//
+
+import Foundation
+
+struct Homeworld: Codable {
+    let name: String
+    let climate: String
+    let terrain: String
+    let population: String
+
+}
