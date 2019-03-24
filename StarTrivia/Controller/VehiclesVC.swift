c3c8b30a09ae8bf431e502dbd880ae29a0169d97
Nikolaos Antonakakis <nikolaos.antonakakis@wu.ac.at>
2019-03-24 22:57:41 +0100

    Vehicles UI

--- StarTrivia/Controller/VehiclesVC.swift
+++ StarTrivia/Controller/VehiclesVC.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class VehiclesVC: UIViewController, PersonProtocol {
     
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var modelLbl: UILabel!
+    @IBOutlet weak var manufacturerLbl: UILabel!
+    @IBOutlet weak var costLbl: UILabel!
+    @IBOutlet weak var lengthLbl: UILabel!
+    @IBOutlet weak var speedLbl: UILabel!
+    @IBOutlet weak var crewLbl: UILabel!
+    @IBOutlet weak var passengersLbl: UILabel!
+    @IBOutlet weak var cargoLbl: UILabel!
+    @IBOutlet weak var consumablesLbl: UILabel!
+    @IBOutlet weak var vehicleClassLbl: UILabel!
+    @IBOutlet weak var pilotLbl: UILabel!
+    @IBOutlet weak var previousBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
     var person: Person!
     
     override func viewDidLoad() {
@@ -18,4 +33,9 @@ class VehiclesVC: UIViewController, PersonProtocol {
 
     }
 
+    @IBAction func previousClicked(_ sender: Any) {
+    }
+    
+    @IBAction func nextClicked(_ sender: Any) {
+    }
 }
