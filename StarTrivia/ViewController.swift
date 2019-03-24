7d4bb3ce3222737c5013f6bb0dcae7af74817940
Nikolaos Antonakakis <nikolaos.antonakakis@wu.ac.at>
2019-03-24 10:41:28 +0100

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Antonakakis Nikolaos on 24.03.19.
+//  Copyright © 2019 Antonakakis Nikolaos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
